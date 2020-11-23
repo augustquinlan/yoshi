d11612c7649beb6eca37b5452e0fc9722383df2c
augustquinlan <74929759+augustquinlan@users.noreply.github.com>
2020-11-23 11:11:39 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    devices.raiseAlertTo(MesAlertEvent.DisplayToast)
+    basic.showString("Yes")
+})
+input.onButtonPressed(Button.B, function () {
+    devices.raiseAlertTo(MesAlertEvent.DisplayToast)
+    basic.showString("No")
 })
